b29013f82103a95f60378dd3bc325b9a92ca7e16
hadimansouri <mohammad.hadi.mansouri@gmail.com>
2019-08-05 20:50:23 +0430

    Add entities: TestRunReport, TestRunTemplate

---
+++ src/TMS/TestRunTemplate.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * 
+ * @author hadi <mohammad.hadi.mansouri@dpq.co.ir>
+ *
+ */
+class TMS_TestRunTemplate extends TMS_DocumentedModelBinary
+{
+
+    /**
+     *
+     * @see Pluf_Model::init()
+     */
+    function init()
+    {
+        parent::init();
+        $this->_model = 'TMS_TestRunTemplate';
+        $this->_a['table'] = 'tms_report_templates';
+        $this->_a['verbose'] = 'TMS Test Run Template';
+        $this->_a['cols'] = array_merge($this->_a['cols'], array(
+
+            /*
+             * Relations
+             */
+            
+        ));
+    }
+    
+}
