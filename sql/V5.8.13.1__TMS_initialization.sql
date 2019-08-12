147929ee9ef5690e0c18e49b0dbb17fa8b30ecd2
hadimansouri <mohammad.hadi.mansouri@gmail.com>
2019-08-12 19:16:12 +0430

    change types of bigint to mediumint or int

--- sql/V5.8.13.1__TMS_initialization.sql
+++ sql/V5.8.13.1__TMS_initialization.sql
@@ -7,16 +7,16 @@
 -- Date:
 -- --------------------------------------------------------------------
 CREATE TABLE `tms_activities` (
-   `id` bigint NOT NULL AUTO_INCREMENT,
+   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
 	`title` varchar(128),
 	`description` varchar(2048),
 	`state` varchar(255),
 	`type` varchar(255),
 	`start_dtime` datetime(6),
 	`end_dtime` datetime(6),
-	`assign_id` bigint,
-	`project_id` bigint NOT NULL,
-	`test_id` bigint NOT NULL,
+	`assign_id` mediumint(9),
+	`project_id` mediumint(9) NOT NULL,
+	`test_id` mediumint(9) NOT NULL,
 	`is_archived` bit NOT NULL,
 	`tenant` mediumint(9) unsigned NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`),
@@ -24,9 +24,9 @@ CREATE TABLE `tms_activities` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `tms_activity_user_account_assoc` (
-	`id` bigint NOT NULL AUTO_INCREMENT,
-	`activity_id` bigint NOT NULL,
-	`account_id` bigint NOT NULL,
+	`id` mediumint(9) NOT NULL AUTO_INCREMENT,
+	`activity_id` mediumint(9) NOT NULL,
+	`account_id` mediumint(9) NOT NULL,
 	`tenant` mediumint(9) unsigned NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`),
 	KEY `tenant_foreignkey_idx` (`tenant`)
@@ -34,12 +34,12 @@ CREATE TABLE `tms_activity_user_account_assoc` (
 
 	
 CREATE TABLE `tms_activity_comments` (
-   `id` bigint NOT NULL AUTO_INCREMENT,
+   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
 	`text` varchar(2048),
 	`mime_type` varchar(255),
 	`creation_dtime` datetime(6) NOT NULL,
-	`writer_id` bigint NOT NULL,
-	`activity_id` bigint NOT NULL,
+	`writer_id` mediumint(9) NOT NULL,
+	`activity_id` mediumint(9) NOT NULL,
 	`tenant` mediumint(9) unsigned NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`),
 	KEY `tenant_foreignkey_idx` (`tenant`)
@@ -47,15 +47,15 @@ CREATE TABLE `tms_activity_comments` (
 
 
 CREATE TABLE `tms_activity_logs` (
-   `id` bigint NOT NULL AUTO_INCREMENT,
+   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
 	`title` varchar(128),
 	`description` varchar(2048),
 	`duration` double precision,
 	`creation_dtime` datetime(6),
-	`writer_id` bigint NOT NULL,
-	`project_id` bigint NOT NULL,
-	`activity_id` bigint NOT NULL,
-	`test_id` bigint NOT NULL,
+	`writer_id` mediumint(9) NOT NULL,
+	`project_id` mediumint(9) NOT NULL,
+	`activity_id` mediumint(9) NOT NULL,
+	`test_id` mediumint(9) NOT NULL,
 	`tenant` mediumint(9) unsigned NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`),
 	KEY `tenant_foreignkey_idx` (`tenant`)
@@ -63,10 +63,10 @@ CREATE TABLE `tms_activity_logs` (
 
 
 CREATE TABLE `tms_activity_outputs` (
-   `id` bigint NOT NULL AUTO_INCREMENT,
+   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
 	`title` varchar(128),
 	`description` varchar(2048),
-	`activity_id` bigint NOT NULL,
+	`activity_id` mediumint(9) NOT NULL,
 	`tenant` mediumint(9) unsigned NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`),
 	KEY `tenant_foreignkey_idx` (`tenant`)
@@ -74,27 +74,27 @@ CREATE TABLE `tms_activity_outputs` (
 
 
 CREATE TABLE `tms_activity_steps` (
-   `id` bigint NOT NULL AUTO_INCREMENT,
+   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
 	`title` varchar(255),
 	`description` varchar(255),
 	`order` integer,
 	`is_checked` bit NOT NULL,
-	`activity_id` bigint NOT NULL,
+	`activity_id` mediumint(9) NOT NULL,
 	`tenant` mediumint(9) unsigned NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`),
 	KEY `tenant_foreignkey_idx` (`tenant`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `tms_projects` (
-   `id` bigint NOT NULL AUTO_INCREMENT,
+   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
 	`title` varchar(128),
 	`description` varchar(2048),
 	`logo` varchar(512),
 	`state` varchar(255),
 	`start_dtime` datetime(6),
 	`end_dtime` datetime(6),
-	`manager_id` bigint,
-	`organization_id` bigint NOT NULL,
+	`manager_id` mediumint(9),
+	`organization_id` mediumint(9) NOT NULL,
 	`tenant` mediumint(9) unsigned NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`),
 	KEY `tenant_foreignkey_idx` (`tenant`)
@@ -102,9 +102,9 @@ CREATE TABLE `tms_projects` (
 
 
 CREATE TABLE `tms_project_user_account_assoc` (
-   `id` bigint NOT NULL AUTO_INCREMENT,
-	`project_id` bigint NOT NULL,
-	`account_id` bigint NOT NULL,
+   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+	`project_id` mediumint(9) NOT NULL,
+	`account_id` mediumint(9) NOT NULL,
 	`tenant` mediumint(9) unsigned NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`),
 	KEY `tenant_foreignkey_idx` (`tenant`)
@@ -112,13 +112,13 @@ CREATE TABLE `tms_project_user_account_assoc` (
 
 
 CREATE TABLE `tms_report_template` (
-   `id` bigint NOT NULL AUTO_INCREMENT,
+   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
 	`title` varchar(255),
 	`description` varchar(255),
 	`mime_type` varchar(255),
 	`file_name` varchar(255),
 	`file_path` varchar(255),
-	`file_size` bigint,
+	`file_size` int(11),
 	`modif_dtime` datetime(6),
 	`tenant` mediumint(9) unsigned NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`),
@@ -127,28 +127,28 @@ CREATE TABLE `tms_report_template` (
 
 
 CREATE TABLE `tms_requirements` (
-   `id` bigint NOT NULL AUTO_INCREMENT,
+   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
 	`title` varchar(128),
 	`description` varchar(2048),
 	`type` varchar(255),
-	`project_id` bigint NOT NULL,
-	`tenant` mediumint(9) unsigned NOT NULL DEFAULT 0,
+	`project_id` mediumint(9) NOT NULL,
+	`tenant` medmediumint(9)(9) unsigned NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`),
 	KEY `tenant_foreignkey_idx` (`tenant`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 CREATE TABLE `tms_scenarios` (
-   `id` bigint NOT NULL AUTO_INCREMENT,
+   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
 	`title` varchar(128),
 	`description` varchar(2048),
 	`type` varchar(128) NOT NULL,
 	`mime_type` varchar(255),
 	`file_name` varchar(255),
 	`file_path` varchar(255),
-	`file_size` bigint,
+	`file_size` int(11),
 	`modif_dtime` datetime(6),
-	`virtual_user_id` bigint NOT NULL,
+	`virtual_user_id` mediumint(9) NOT NULL,
 	`tenant` mediumint(9) unsigned NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`),
 	KEY `tenant_foreignkey_idx` (`tenant`)
@@ -156,7 +156,7 @@ CREATE TABLE `tms_scenarios` (
 
 
 CREATE TABLE `tms_test_acceptance_criteria` (
-   `id` bigint NOT NULL AUTO_INCREMENT,
+   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
 	`title` varchar(128),
 	`description` varchar(2048),
 	`metric` varchar(64),
@@ -165,7 +165,7 @@ CREATE TABLE `tms_test_acceptance_criteria` (
 	`duration` double precision NOT NULL,
 	`duration_type` varchar(255) NOT NULL,
 	`severity` varchar(255) NOT NULL,
-	`test_id` bigint NOT NULL,
+	`test_id` mediumint(9) NOT NULL,
 	`tenant` mediumint(9) unsigned NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`),
 	KEY `tenant_foreignkey_idx` (`tenant`)
@@ -173,15 +173,15 @@ CREATE TABLE `tms_test_acceptance_criteria` (
 
 
 CREATE TABLE `tms_test_attachments` (
-   `id` bigint NOT NULL AUTO_INCREMENT,
+   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
 	`title` varchar(128),
 	`description` varchar(2048),
 	`mime_type` varchar(255),
 	`file_name` varchar(255),
 	`file_path` varchar(255),
-	`file_size` bigint,
+	`file_size` int(11),
 	`modif_dtime` datetime(6),
-	`test_id` bigint NOT NULL,
+	`test_id` mediumint(9) NOT NULL,
 	`tenant` mediumint(9) unsigned NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`),
 	KEY `tenant_foreignkey_idx` (`tenant`)
@@ -189,12 +189,12 @@ CREATE TABLE `tms_test_attachments` (
 
 
 CREATE TABLE `tms_test_comments` (
-   `id` bigint NOT NULL AUTO_INCREMENT,
+   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
 	`text` varchar(2048),
 	`mime_type` varchar(255),
 	`creation_dtime` datetime(6) NOT NULL,
-	`writer_id` bigint NOT NULL,
-	`test_id` bigint NOT NULL,
+	`writer_id` mediumint(9) NOT NULL,
+	`test_id` mediumint(9) NOT NULL,
 	`tenant` mediumint(9) unsigned NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`),
 	KEY `tenant_foreignkey_idx` (`tenant`)
@@ -202,12 +202,12 @@ CREATE TABLE `tms_test_comments` (
 
 
 CREATE TABLE `tms_test_histories` (
-   `id` bigint NOT NULL AUTO_INCREMENT,
+   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
 	`action` varchar(64),
 	`title` varchar(128),
 	`creation_dtime` datetime(6),
-	`account_id` bigint,
-	`test_id` bigint,
+	`account_id` mediumint(9),
+	`test_id` mediumint(9),
 	`tenant` mediumint(9) unsigned NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`),
 	KEY `tenant_foreignkey_idx` (`tenant`)
@@ -215,9 +215,9 @@ CREATE TABLE `tms_test_histories` (
 
 
 CREATE TABLE `tms_requirenment_tms_test_assoc` (
-   `id` bigint NOT NULL AUTO_INCREMENT,
-	`requirenment_id` bigint NOT NULL,
-	`test_id` bigint NOT NULL,
+   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+	`requirenment_id` mediumint(9) NOT NULL,
+	`test_id` mediumint(9) NOT NULL,
 	`tenant` mediumint(9) unsigned NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`),
 	KEY `tenant_foreignkey_idx` (`tenant`)
@@ -225,12 +225,12 @@ CREATE TABLE `tms_requirenment_tms_test_assoc` (
 
 
 CREATE TABLE `tms_test_risks` (
-   `id` bigint NOT NULL AUTO_INCREMENT,
+   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
 	`title` varchar(128),
 	`description` varchar(2048),
 	`effect` varchar(1024),
 	`probability` double precision,
-	`test_id` bigint NOT NULL,
+	`test_id` mediumint(9) NOT NULL,
 	`tenant` mediumint(9) unsigned NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`),
 	KEY `tenant_foreignkey_idx` (`tenant`)
@@ -238,17 +238,17 @@ CREATE TABLE `tms_test_risks` (
 
 
 CREATE TABLE `tms_test_run_reports` (
-   `id` bigint NOT NULL AUTO_INCREMENT,
+   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
 	`title` varchar(255),
 	`description` varchar(255),
 	`mime_type` varchar(255),
 	`file_name` varchar(255),
 	`file_path` varchar(255),
-	`file_size` bigint,
-	`downloads` bigint,
+	`file_size` int(11),
+	`downloads` int(11),
 	`creation_dtime` datetime(6),
 	`modif_dtime` datetime(6),
-	`test_run_id` bigint,
+	`test_run_id` mediumint(9),
 	`tenant` mediumint(9) unsigned NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`),
 	KEY `tenant_foreignkey_idx` (`tenant`)
@@ -256,7 +256,7 @@ CREATE TABLE `tms_test_run_reports` (
 
 
 CREATE TABLE `tms_test_runs` (
-   `id` bigint NOT NULL AUTO_INCREMENT,
+   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
 	`title` varchar(100),
 	`description` varchar(2048),
 	`start_dtime` datetime(6),
@@ -267,8 +267,8 @@ CREATE TABLE `tms_test_runs` (
 	`file_path` VARCHAR(255) NULL,
 	`file_size` BIGINT NULL,
 	`modif_dtime` DATETIME(6) NULL,
-	`test_id` bigint NOT NULL,
-	`pipeline_id` bigint,
+	`test_id` mediumint(9) NOT NULL,
+	`pipeline_id` mediumint(9),
 	`tenant` mediumint(9) unsigned NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`),
 	KEY `tenant_foreignkey_idx` (`tenant`)
@@ -276,9 +276,9 @@ CREATE TABLE `tms_test_runs` (
 
 
 CREATE TABLE `tms_test_user_account_assoc` (
-   `id` bigint NOT NULL AUTO_INCREMENT,
-	`test_id` bigint NOT NULL,
-	`account_id` bigint NOT NULL,
+   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+	`test_id` mediumint(9) NOT NULL,
+	`account_id` mediumint(9) NOT NULL,
 	`tenant` mediumint(9) unsigned NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`),
 	KEY `tenant_foreignkey_idx` (`tenant`)
@@ -286,11 +286,11 @@ CREATE TABLE `tms_test_user_account_assoc` (
 
 
 CREATE TABLE `tms_test_variables` (
-   `id` bigint NOT NULL AUTO_INCREMENT,
+   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
 	`key` varchar(128),
 	`value` varchar(1024),
 	`description` varchar(1024),
-	`test_id` bigint,
+	`test_id` mediumint(9),
 	`tenant` mediumint(9) unsigned NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`),
 	KEY `tenant_foreignkey_idx` (`tenant`)
@@ -298,7 +298,7 @@ CREATE TABLE `tms_test_variables` (
 
 
 CREATE TABLE `tms_tests` (
-   `id` bigint NOT NULL AUTO_INCREMENT,
+   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
 	`title` varchar(128),
 	`description` varchar(2048),
 	`type` varchar(255),
@@ -308,8 +308,8 @@ CREATE TABLE `tms_tests` (
 	`start_dtime` datetime(6),
 	`end_dtime` datetime(6),
 	`is_accepted` bit,
-	`project_id` bigint NOT NULL,
-	`responsible_id` bigint,
+	`project_id` mediumint(9) NOT NULL,
+	`responsible_id` mediumint(9),
 	`tenant` mediumint(9) unsigned NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`),
 	KEY `tenant_foreignkey_idx` (`tenant`)
@@ -317,16 +317,16 @@ CREATE TABLE `tms_tests` (
 
 
 CREATE TABLE `tms_virtual_users` (
-   `id` bigint NOT NULL AUTO_INCREMENT,
+   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
 	`title` varchar(128),
 	`description` varchar(2048),
 	`type` varchar(128) NOT NULL,
 	`mime_type` varchar(255),
 	`file_name` varchar(255),
 	`file_path` varchar(255),
-	`file_size` bigint,
+	`file_size` int(11),
 	`modif_dtime` datetime(6),
-	`test_id` bigint NOT NULL,
+	`test_id` mediumint(9) NOT NULL,
 	`tenant` mediumint(9) unsigned NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`),
 	KEY `tenant_foreignkey_idx` (`tenant`)
