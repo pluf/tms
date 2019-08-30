fbad2eccd0b0535273c1b3419ef9bd17111896a7
mostafabarmshory <mostafa.barmshory@dpq.co.ir>
2019-08-30 14:41:37 +0430

    Update PHPUnit version in the group

--- tests/Basic/ModelTest.php
+++ tests/Basic/ModelTest.php
@@ -30,7 +30,7 @@ class Basic_ModelTest extends TestCase
     /**
      * @before
      */
-    public function setUp ()
+    public function setUpTest ()
     {
         Pluf::start(__DIR__. '/../conf/config.php');
     }
