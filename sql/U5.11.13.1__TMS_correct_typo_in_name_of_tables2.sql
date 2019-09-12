17c5f609fcd55e0672610e671e6985431106b240
hadimansouri <mohammad.hadi.mansouri@gmail.com>
2019-09-12 11:17:53 +0430

    correct typo in the name of the association table between requirement and test

---
+++ sql/U5.11.13.1__TMS_correct_typo_in_name_of_tables2.sql
@@ -0,0 +1,4 @@
+/*
+* Rename command is not an SQL standard command. The following command will work on MySQL.
+*/
+RENAME TABLE `tms_requirement_tms_test_assoc` TO `tms_requirenment_tms_test_assoc`;
