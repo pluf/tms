3d79c5845af3d87ca1e362da743cbbfb3f8f14d3
hadimansouri <mohammad.hadi.mansouri@gmail.com>
2019-09-14 21:33:54 +0430

    Add sql script to rename FK names in the associated tables

---
+++ sql/V5.11.13.3__TMS_rename_fk_fields_in_associated_tables.sql
@@ -0,0 +1,12 @@
+
+ALTER TABLE `tms_activity_user_account_assoc` CHANGE `activity_id` `tms_activity_id` mediumint(9) unsigned NOT NULL;
+ALTER TABLE `tms_activity_user_account_assoc` CHANGE `account_id` `user_account_id` mediumint(9) unsigned NOT NULL;
+
+ALTER TABLE `tms_project_user_account_assoc` CHANGE `project_id` `tms_project_id` mediumint(9) unsigned NOT NULL;
+ALTER TABLE `tms_project_user_account_assoc` CHANGE `account_id` `user_account_id` mediumint(9) unsigned NOT NULL;
+
+ALTER TABLE `tms_requirenment_tms_test_assoc` CHANGE `requirenment_id` `tms_requirenment_id` mediumint(9) unsigned NOT NULL;
+ALTER TABLE `tms_requirenment_tms_test_assoc` CHANGE `test_id` `tms_test_id` mediumint(9) unsigned NOT NULL;
+
+ALTER TABLE `tms_test_user_account_assoc` CHANGE `test_id` `tms_test_id` mediumint(9) unsigned NOT NULL;
+ALTER TABLE `tms_test_user_account_assoc` CHANGE `account_id` `user_account_id` mediumint(9) unsigned NOT NULL;
