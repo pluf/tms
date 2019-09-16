70ab22848aebe83e3991ec732e39555e9d3e48bd
hadimansouri <mohammad.hadi.mansouri@gmail.com>
2019-09-16 14:08:46 +0430

    Add unit tests for scenario

---
+++ tests/RestScenario/AnonymousTest.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ * This file is part of Pluf Framework, a simple PHP Application Framework.
+ * Copyright (C) 2010-2020 Phoinex Scholars Co. (http://dpq.co.ir)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace RestScenario;
+
+use \Basic_AbstractAnonymousNotAllowedTest;
+
+require_once 'Pluf.php';
+set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__ . '/../Base/');
+
+/**
+ *
+ * @backupGlobals disabled
+ * @backupStaticAttributes disabled
+ */
+class AnonymousTest extends Basic_AbstractAnonymousNotAllowedTest
+{
+
+    public function createApiV2()
+    {
+        $myAPI = array(
+            array(
+                'app' => 'Tenant',
+                'regex' => '#^/api/v2/tms#',
+                'base' => '',
+                'sub' => include 'TMS/urls.php'
+            ),
+            array(
+                'app' => 'User',
+                'regex' => '#^/api/v2/user#',
+                'base' => '',
+                'sub' => include 'User/urls-v2.php'
+            )
+        );
+        return $myAPI;
+    }
+
+    public function getBaseUrl()
+    {
+        return '/api/v2/tms/scenarios';
+    }
+}
+
+
