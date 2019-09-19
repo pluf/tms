0dec9b1934e6ac427ae49286bfc990eec3a209e9
hadimansouri <mohammad.hadi.mansouri@gmail.com>
2019-09-19 14:31:03 +0430

    add some utilities to create pipeline for each test run

---
+++ src/TMS/ScriptBuilder.php
@@ -0,0 +1,36 @@
+<?php
+
+class TMS_ScriptBuilder
+{
+
+    private $lines = array();
+
+    public function addCommand($command)
+    {
+        $this->lines->push($command);
+    }
+
+    public function addComment($comment)
+    {
+        $str = str_replace('\n', '\n#', $comment);
+        $this->addCommand('#' . $str);
+    }
+
+    public function buildFile($file)
+    {
+        $fp = fopen($file, 'a');//opens file in append mode
+        foreach($this->lines as $line){
+            fwrite($fp, $line . '\n');
+        }
+        fclose($fp);
+    }
+    
+    public function buildString(){
+        $string = '';
+        foreach($this->lines as $line){
+            $string .= $line . '\n';
+        }
+        return $string;
+    }
+}
+
